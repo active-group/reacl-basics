0570bfc48b476f929ea0e0190aae05ffbc2ba869
David Frese <david.frese@active-group.de>
2019-02-25 00:01:14 +0100

    Remove browser.native module.

--- src/reacl_basics/actions/browser.cljs
+++ src/reacl_basics/actions/browser.cljs
@@ -1,20 +1,20 @@
 (ns reacl-basics.actions.browser
   (:require [reacl-basics.actions.core :as core]
-            [reacl-basics.actions.browser.native :as native]))
+            goog.async.nextTick))
 
 (defn clear-timeout [id]
-  (core/external-action native/clear-timeout! id))
+  (core/external-action js/window.clearTimeout js/window id))
 
 (letfn [(tmo [send! make-id-message ms message]
-          (make-id-message (native/set-timeout! ms (fn []
-                                                     (send! message)))))]
+          (make-id-message (js/window.setTimeout (fn []
+                                                   (send! message)) ms)))]
   (defn timeout
     [target make-id-message ms message]
     (core/message-action target tmo make-id-message ms message)))
 
 (letfn [(ntk [send! message]
-          (native/next-tick! (fn []
-                               (send! message))))]
+          (goog.async.nextTick (fn []
+                                 (send! message))))]
   (defn next-tick [target message]
     ;; Note: next-tick cannot be cancelled. (don't know if that is a flaw in goog.async.nextTick or not)
     (core/message-action target ntk message)))
@@ -27,9 +27,9 @@
     prev-id (core/comp-actions (clear-timeout prev-id))))
 
 (defn go-back []
-  (core/external-action native/go-back!))
+  (core/external-action js/window.history.back))
 
 (defn reload
   ([] (reload false))
   ([clear-cache?]
-   (core/external-action native/reload! (boolean clear-cache?))))
+   (core/external-action js/window.location.reload (boolean clear-cache?))))
