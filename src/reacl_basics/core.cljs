35056e8a58c7b2981b5f2d4a3974f4878faa56d6
David Frese <david.frese@active-group.de>
2019-02-24 23:38:53 +0100

    Add 'return' utilities.

--- src/reacl_basics/core.cljs
+++ src/reacl_basics/core.cljs
@@ -36,3 +36,46 @@
   (identity (reacl/opt :reduce-action
                        (apply action-reducer f args))
             elem))
+
+(defn- return-tuples [& l]
+  (apply reacl/return (apply concat l)))
+
+(defn- returned-tuples [r]
+  ;; FIXME: reacl should expose something for this:
+  (assert (instace? reacl/Effects r))
+  (let [v (vec (:args r))]
+    (assert (even? (count v)))
+    (map (fn [i]
+           [(get prev i) (get prev (inc i))])
+         (map #(* 2 %)
+              (range (/ (count prev) 2))))))
+
+(defn- returned-state [r st]
+  (or (second (first (filter #(= st (first %))
+                             (returned-tuples r))))
+      reacl/keep-state))
+
+(defn returned-app-state [r]
+  (returned-state r :app-state))
+
+(defn returned-local-state [r]
+  (returned-state r :local-state))
+
+(defn returned-actions [r]
+  (map second (filter #(= :action (first %))
+                      (returned-tuples r))))
+
+(defn- set-returned-state [r st v]
+  (return-tuples (cons [st v]
+                       (remove #(= st (first %))
+                               (returned-tuples r)))))
+
+(defn set-returned-app-state [r v]
+  (set-returned-state r :app-state v))
+
+(defn set-returned-local-state [r v]
+  (set-returned-state r :local-state v))
+
+(defn add-returned-action [r action]
+  (return-tuples (concat (returned-tuples r)
+                         [:action action])))
