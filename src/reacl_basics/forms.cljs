7a13c075516d59707ffa416eb8794a9d48539c6e
David Frese <david.frese@active-group.de>
2019-02-25 00:04:54 +0100

    Rename 'dom' as 'forms'.

--- src/reacl_basics/forms.cljs
+++ src/reacl_basics/forms.cljs
@@ -1,8 +1,8 @@
-(ns reacl-basics.dom
-  "Redefines the some dom elements as classes that offer app-states
-  for the content usually editable by a user, and/or actions for the
+(ns reacl-basics.forms
+  "Redefines some dom elements as classes that offer app-states
+  for the content usually editable by a user in forms, and/or actions for the
   most common discrete user interactions."
-  (:require [reacl2.dom :as rdom]
+  (:require [reacl2.dom :as dom]
             [reacl-basics.core :as c :include-macros true]
             [reacl-basics.classes :as cl]))
 
@@ -11,13 +11,13 @@
   `content`, and emits `true` as an action when clicked. It also sets `:type` to \"button\"."
   ([& content] [attrs & content])
   (cl/input-class "button"
-                  (fn [attrs] (rdom/button (merge {:type "button"} attrs)))
+                  (fn [attrs] (dom/button (merge {:type "button"} attrs)))
                   :onclick (constantly true)))
 
 (c/defn-pseudo input-value
   "An app-state class that renders as a `dom/input` with a `value` corresponding to its app-state."
   ([] [attrs])
-  (cl/interactive-value-class "input-value" rdom/input))
+  (cl/interactive-value-class "input-value" dom/input))
 
 (c/defc-dom input-text
   "An app-state class that renders as a `dom/input` with `:type` \"text\", with a `value` corresponding to its app-state."
@@ -33,14 +33,14 @@
   as an `dom/input` with `:type` \"checkbox\" representing the
   app-state."
   ([] [attrs])
-  (cl/interactive-checked-class "input-checkbox" "checkbox" rdom/input))
+  (cl/interactive-checked-class "input-checkbox" "checkbox" dom/input))
 
 (c/defn-pseudo input-radio
   "An app-state class with an optional `attrs` argument, which renders
   as an `dom/input` with `:type` \"radio\" representing the
   app-state."
   ([] [attrs])
-  (cl/interactive-checked-class "input-radio" "radio" rdom/input))
+  (cl/interactive-checked-class "input-radio" "radio" dom/input))
 
 (c/defn-pseudo form
   "A non-app-state class with an optional `attrs` and arbitrary
@@ -49,29 +49,29 @@
    when reset."
   ([& content] [attrs & content])
   (cl/input-class ""
-                  rdom/form
+                  dom/form
                   :onsubmit (constantly true)
                   :onreset (constantly false)))
 
 (c/defn-dom button-submit
   "Returns a `dom/button` element with optional `attrs` and `content`, and `:type` \"submit\"."
   [attrs & content]
-  (apply rdom/button (merge {:type "submit"} attrs) content))
+  (apply dom/button (merge {:type "submit"} attrs) content))
 
 (c/defn-dom button-reset
   "Returns a `dom/button` element with optional `attrs` and `content`, and `:type` \"reset\"."
   [attrs & content]
-  (apply rdom/button (merge {:type "reset"} attrs) content))
+  (apply dom/button (merge {:type "reset"} attrs) content))
 
 (c/defn-pseudo textarea
   "An app-state class with an optional `attrs` argument, which renders
   as a `dom/textarea` with the app-state as the editable content."
   ([] [attrs])
-  (cl/interactive-value-class "textarea" rdom/text-area))
+  (cl/interactive-value-class "textarea" dom/text-area))
 
 (c/defn-pseudo select
   "A non-app-state class with an optional `attrs` argument and
   arbitrary `content` elements, which renders as a `dom/select` where
   the `:value` attribute represents the app-state."
   ([& content] [attrs & content])
-  (cl/interactive-value-class "select" rdom/select))
+  (cl/interactive-value-class "select" dom/select))
