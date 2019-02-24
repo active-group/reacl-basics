ed412a36247bfb8ff4e99310e9e4bd287475dde3
David Frese <david.frese@active-group.de>
2019-02-24 23:38:37 +0100

    Add 'reduce-action' as a function over an element.

--- src/reacl_basics/core.cljs
+++ src/reacl_basics/core.cljs
@@ -1,5 +1,6 @@
 (ns reacl-basics.core
-  (:require [reacl2.core :as reacl]))
+  (:require [reacl2.core :as reacl])
+  (:refer-clojure :exclude [identity]))
 
 (defn ^:no-doc attributes? [v] ;; TODO: should be public in reacl.
   (and (map? v)
@@ -18,3 +19,20 @@
     (apply f args)
     (apply f {} args)))
 
+(reacl/defclass ^{:doc "A non-app-state class that does nothing but render as `content`."}
+  identity
+  this [content]
+  render content)
+
+(defrecord ^:no-doc ActionReducer [f args]
+  IFn
+  (-invoke [this state action]
+    (apply f state action args)))
+
+(defn action-reducer [f & args]
+  (ActionReducer. f args))
+
+(defn reduce-action [elem f & args]
+  (identity (reacl/opt :reduce-action
+                       (apply action-reducer f args))
+            elem))
