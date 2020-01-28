47d4d9460050963ac949dc8ff5918441d2406c84
David Frese <david.frese@active-group.de>
2020-01-28 11:23:46 +0100

    Bugfix in finding a matching route.

--- src/reacl_basics/pages/ring.clj
+++ src/reacl_basics/pages/ring.clj
@@ -6,8 +6,7 @@
    (wrap-client-fn-routes handler @routes/defined-routes client-fn))
   ([handler routes client-fn]
    (fn [request]
-     (if-let [route (first (filter some? (map #(routes/route-matches % request)
-                                              routes)))]
+     (if-let [route (first (filter #(routes/route-matches % request) routes))]
        ;; TODO: really call client-fn with route?
        (apply client-fn route (routes/route-matches route request))
        (handler request)))))
